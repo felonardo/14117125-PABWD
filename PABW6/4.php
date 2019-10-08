28f5fcaf8058c585aff9fb6c4995e0e53409b16a
Leonardo <mozarelast89@gmail.com>
2019-10-08 14:27:19 +0700

    minggu 6

---
+++ PABW6/4.php
@@ -0,0 +1,12 @@
+<?php
+$n = 37;
+// $n = isset($_GET['n']) ? $_GET['n'] : 1;
+for ($angka = 1; $angka <= $n; $angka++) {
+    $prima = true;
+    for ($i = 2; $i < $angka; $i++) {
+        if ($angka % $i == 0)
+            $prima = false;
+    }
+    if ($prima)
+        echo "$angka ";
+}
